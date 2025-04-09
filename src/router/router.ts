9218be726ccd0a9386ef53404f290463bdb22546
Nikita Sheremet <nikita.sheremet253@gmail.com>
2025-04-08 20:40:30 -0400

    fix: when github pages is refreshed

--- src/router/router.ts
+++ src/router/router.ts
@@ -14,7 +14,9 @@ const routes = [
   },
 ];
 
-export const router = createRouter({
-  history: createWebHistory("/TaleGrid/"),
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
   routes,
 });
+
+export { router };
